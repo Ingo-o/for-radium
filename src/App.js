3e55fb1c303651223b77bdf8f4e77fe39c462f74
Ingo <ingo-o@yandex.ru>
2022-01-19 22:29:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import classNames from "classnames";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [colorMode, changeColorMode] = useState(false);
+    const [invisibleMode, changeInvisibleMode] = useState(false);
+    const [inscriptionExistence, changeInscriptionExistence] = useState(true);
+
+    const toggleColorMode = () => {
+        changeColorMode(!colorMode);
+    }
+
+    const toggleInvisibleMode = (event) => {
+        event.preventDefault()
+        changeInvisibleMode(!invisibleMode)
+    }
+
+    const toggleInscriptionExistence = (event) => {
+        if (event.key === " ") {
+            changeInscriptionExistence(!inscriptionExistence);
+        }
+    }
+
+    return (
+        <div className="App"
+             onClick={toggleInvisibleMode}
+             onContextMenu={toggleInvisibleMode}
+             onKeyDown={toggleInscriptionExistence}
+             tabIndex={'0'}
+             >
+            {inscriptionExistence
+                ? <p onPointerEnter={toggleColorMode} onPointerLeave={toggleColorMode}
+                className={classNames({"colorModeOn": colorMode}, {"invisibleModeOn": invisibleMode})}>
+                Дмитрий Епихин <br/> Начинающий web-программист / Junior Frontend Developer</p>
+                : <p/>}
+        </div>
+    );
 }
 
 export default App;
